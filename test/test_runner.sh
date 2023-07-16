350b486e90964bc8a6a4955fd17891b4d83cd540
Sherif Kunch <sherifkunch@MBP-00007.local>
2023-07-16 19:28:32 +0300

    test finished

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -4,3 +4,9 @@ export BACKEND=`docker-compose ps -q backend`
 
 docker exec ${BACKEND} ls | grep "main.go"
 
+# Perform a health check by sending a GET request to a known endpoint
+curl -sSf -x localhost:80 http://backend:8000 | grep "Blog post #0"
+if [[ $? != 0 ]]; then
+  echo "Proxy is not healthy"
+  exit 1
+fi
