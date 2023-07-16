3047f79966ca9bab410e8c85e44cb4f5765e59e8
Sherif Kunch <sherifkunch@MBP-00007.local>
2023-07-16 18:24:24 +0300

    test

--- build.sh
+++ build.sh
@@ -2,10 +2,7 @@
 
 # Build Docker containers using Docker Compose
 echo "Building Docker containers..."
-
-echo "Build backend"
-cd backend
-docker build . --file Dockerfile --tag my-image-name:$(date +%s)
+docker-compose build --pull --no-cache
 
 sleep 5 
 
@@ -14,12 +11,10 @@ docker-compose up -d
 # Check if the build was successful
 if [ $? -eq 0 ]; then
   echo "Docker containers built successfully."
-
-  sleep 10 
-
+   sleep 5
   # Run the test runner script
   echo "Running tests..."
-  ./test/test_runner.sh
+  .test/test_runner.sh
 
   # Check the exit code of the test runner script
   if [ $? -eq 0 ]; then
