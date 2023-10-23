67826c7c2d51200bf0f530c6759f9c7f264e8203
Sherif Kunch <sherifkunch@MBP-00007.local>
2023-10-23 16:04:01 +0300

    test

--- test/test_runner.sh
+++ test/test_runner.sh
@@ -1,16 +1,49 @@
 #!/bin/bash
-docker-compose ps -a
 
-export BACKEND=`docker-compose ps -q backend`
+#!/bin/bash
 
-docker exec ${BACKEND} ls | grep "main.go"
+# Define the names of your Docker containers
+container_names=("proxy" "backend" "database")
 
-# Perform a health check by sending a GET request to a known endpoint
-curl -v -sSf -x 127.0.0.1:80 http://backend:8000 | grep "Blog post #0"
-if [[ $? != 0 ]]; then
-  echo "Proxy is not healthy"
-  exit 1
-fi
+# Function to check if a container is running
+container_is_running() {
+  local container_name="$1"
+  docker ps --format '{{.Names}}' | grep -q "^$container_name$"
+}
+
+# Function to check if a container is built
+container_is_built() {
+  local image_name="sherifkunch/go-app"
+  docker images --format '{{.Repository}}' | grep -q "^$image_name$"
+}
+
+# Iterate through the container names and check if they are built and running
+for container_name in "${container_names[@]}"; do
+  if container_is_running "$container_name"; then
+    echo "Container $container_name is running."
+  else
+    echo "Container $container_name is NOT running."
+    exit 1
+  fi
+
+  if container_is_built "$container_name"; then
+    echo "Image for $container_name is built."
+  else
+    echo "Image for $container_name is NOT built."
+    exit 1
+  fi
+done
+
+# export BACKEND=`docker-compose ps -q backend`
+
+# docker exec ${BACKEND} ls | grep "main.go"
+
+# # Perform a health check by sending a GET request to a known endpoint
+# curl -v -sSf -x 127.0.0.1:80 http://backend:8000 | grep "Blog post #0"
+# if [[ $? != 0 ]]; then
+#   echo "Proxy is not healthy"
+#   exit 1
+# fi
 
 # we can think of many other tests here, this one above is just an example
 # moreover, there are helpful tools which can be used to create such tests, but for simplicity I added just a simple one
